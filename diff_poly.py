a4b7b877e1e8a2f2ef9ec0bd69866a2db26d62e1
Shadab Zafar <dufferzafar0@gmail.com>
2015-01-07 16:58:37 +0530

    Updated the list of irreducible polynomials

--- diff_poly.py
+++ diff_poly.py
@@ -7,9 +7,11 @@ By testing out different irreducible polynomials and affine constants.
 import core
 
 # A list of irreducible polynomials and constants
-irreducibles = [0x11B, 0x11D, 0x12B, 0x139, 0x13F, 0x14D, 0x15F, 0x163, 0x165,
-                0X169, 0X171, 0X177, 0X17B, 0X187, 0X18B, 0X18D, 0X19F, 0X1A3,
-                0X1A9, 0X1C3, 0X1CF, 0X1D7, 0X1DD, 0X1E7, 0X1F3, 0X1F5, 0X1F9]
+# http://www.sciencedirect.com/science/article/pii/S2212017313006051
+irreducibles = [0x11B, 0x11D, 0x12B, 0x12D, 0x139, 0x13F, 0x14D, 0x15F, 0x163,
+                0x165, 0x169, 0x171, 0x177, 0x17B, 0x187, 0x18B, 0x18D, 0x19F,
+                0x1A3, 0x1A9, 0x1B1, 0x1BD, 0x1C3, 0x1CF, 0x1D7, 0x1DD, 0x1E7,
+                0x1F3, 0x1F5, 0x1F9]
 
 constants = [0X0A, 0X0F, 0X15, 0X2A, 0X2B, 0X31, 0X32, 0X35, 0X38, 0X40, 0X4A,
              0X4E, 0X54, 0X5E, 0X62, 0X6E, 0X74, 0X7E, 0XF5, 0XF0, 0XEA, 0XD5,
