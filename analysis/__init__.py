663807a0f5ab6cd6706731b2f5d568021cf3098e
Shadab Zafar <dufferzafar0@gmail.com>
2015-04-26 12:59:11 +0530

    Refactored analysis code
    So we don't have to import all the functions in other files.

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,3 +1,29 @@
 # Import all functions here
 from .diffprob import differential_probability
+from .nonlin import non_linearity
+from .degree import algebraic_degree
 from .bijective import is_bijective
+
+
+def values(sbox):
+    """ Calculate standard parameters. """
+    nl = non_linearity(sbox)
+    dp = differential_probability(sbox)
+    bj = str(is_bijective(sbox))
+
+    return (nl, dp, bj)
+
+
+def report(sbox):
+    """ Create a report based on all calculated parameters. """
+
+    out = """
+Analysis
+========
+
+Non Linearity: %d
+Differential Probability: %d
+Is Bijective? %s
+    """
+
+    print(out % values(sbox))
