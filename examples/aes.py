0cd0af9310192d032768ecbe8997249d63148c3f
Shadab Zafar <dufferzafar0@gmail.com>
2015-04-26 13:04:31 +0530

    Updated the AES example

--- examples/aes.py
+++ examples/aes.py
@@ -1,39 +1,31 @@
 """ The standard S-Box used in Rijndael. """
 
-# Because the core is present
-# in parent directory.
+from functools import partial
+from os.path import abspath, dirname
+
+# Because the core is present in parent directory.
 from sys import path
-path.insert(1, '..')
+path.insert(1, dirname(dirname(abspath(__file__))))
 
 import core
-
-from analysis import (
-    differential_probability,
-    non_linearity,
-    algebraic_degree
-)
-
-# Standard parameters
-irreducible_polynomial = 0x11B
-affine_constant = 0x63
+import analysis
 
 # A reduced version of a polynomial is an integer
 # with the MSB cleared So, 0x11B becomes 0x1B and
 # 0x169 becomes 0x69.
 # These are used during multiplication
 # Todo: Understand these completely.
-reduced = core._B2I(core._I2B(irreducible_polynomial)[1:])
+reduced = core._B2I(core._I2B(0x11B)[1:])
+
+affine = partial(core.affine, u=0x1F, v=0x63)
+power = partial(core.inverse, r=reduced)
 
 sbox = []
 
-# Apply transformations on all element
+# Apply transformations on all elements
 for i in range(256):
-    sbox.append(core.affine(core.inverse(i, reduced), affine_constant))
+    sbox.append(affine(power(i)))
 
 # The Box
 print(core.pretty(sbox))
-
-# Analysis
-print("Differential Probability: %d" % differential_probability(sbox))
-# print("Non Linearity: %d" % non_linearity(sbox))
-print("Algebraic Degree: %d" % algebraic_degree(sbox))
+analysis.report(sbox)
