d0949fe66a1701dc51bd987704325cb9b5581218
Shadab Zafar <dufferzafar0@gmail.com>
2015-10-26 20:26:12 +0530

    Update Mobius!

--- examples/mobius.py
+++ examples/mobius.py
@@ -8,17 +8,7 @@ from sys import path
 path.insert(1, '..')
 
 import core
-
-from analysis import (
-    differential_probability,
-    is_bijective,
-)
-
-# Standard parameters
-irreducible_polynomial = 0x11B
-constant = 0x63
-
-reduced = core._B2I(core._I2B(irreducible_polynomial)[1:])
+import analysis
 
 
 def mobius_transform(z, a, b, c, d):
@@ -26,21 +16,20 @@ def mobius_transform(z, a, b, c, d):
     denominator = core._multiply(c, z, reduced) ^ d
     return core._multiply(numerator, core.inverse(denominator, reduced), reduced)
 
+reduced = core._B2I(core._I2B(0x11B)[1:])
+
 # Partials are fun!
-affine = partial(core.affine, c=constant)
-power = partial(core.inverse, r=irreducible_polynomial)
+affine = partial(core.affine, u=0x1A, v=0x63)
+power = partial(core.inverse, r=reduced)
 mobius = partial(mobius_transform, a=33, b=23, c=12, d=9)
 
 sbox = []
 
 # Apply transformations on all element
 for i in range(256):
-    sbox.append((mobius(i)))
-    # sbox.append(affine(power(mobius(i))))
+    # sbox.append(mobius(i))
+    sbox.append(affine(power(mobius(i))))
 
 # The Box
 print(core.pretty(sbox))
-
-# Analysis
-print("Differential Probability: %d" % differential_probability(sbox))
-print("Is Bijective? " + str(is_bijective(sbox)))
+analysis.report(sbox)
